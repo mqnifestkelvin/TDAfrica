406fccc1655906e659fe905d6824b60b010c0e80
Manifest Chakalov <31442656+mqnifestkelvin@users.noreply.github.com>
2023-06-26 02:09:22 +0100

    Update elem.py

--- elem.py
+++ elem.py
@@ -16,3 +16,22 @@ def build_xml_elements(parent_element, data):
             else:
                 element = ET.SubElement(parent_element, key)
                 element.text = str(value)
+
+def build_xml_element(parent, data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key.startswith("@"):
+                    attribute_name = key[1:]
+                    parent.set(attribute_name, str(value))
+                elif isinstance(value, dict):
+                    element = ET.SubElement(parent, key.replace(" ", "_"))
+                    build_xml_element(element, value)
+                elif isinstance(value, list):
+                    for item in value:
+                        subelement = ET.SubElement(parent, key.replace(" ", "_"))
+                        build_xml_element(subelement, item)
+                else:
+                    element = ET.SubElement(parent, key.replace(" ", "_"))
+                    element.text = str(value)
+        else:
+            parent.text = str(data)
